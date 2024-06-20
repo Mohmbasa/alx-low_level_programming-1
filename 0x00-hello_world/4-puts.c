074ff6fd868ef049f409b67f4a34011793b4259d
Basem-ashraf <basemashraf246@gmail.com>
2024-06-20 16:53:22 -0700

    C put task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  *   main - main block
  *   Return: always (0)
- *   */
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
