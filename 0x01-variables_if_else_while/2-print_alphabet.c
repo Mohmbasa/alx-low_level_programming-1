85e6bbc863a274cf8aa6e06b4fed0da019042f58
Basem-ashraf <basemashraf246@gmail.com>
2024-06-21 19:23:42 -0700

    easy

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - start point
+ *
+ * return : 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
