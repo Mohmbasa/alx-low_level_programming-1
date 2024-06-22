52499968e952bf0538080f76ab69d19f40f80d76
Basem-ashraf <basemashraf246@gmail.com>
2024-06-21 19:35:14 -0700

    hope1

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h> 
+/**
+ * main - entry point
+ *
+ * return : always 0 (sucess)
+ */
+int mian(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'A' ; a <= 'Z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+	return (0);
+}
